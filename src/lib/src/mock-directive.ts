cd3255450d1376131b104278fc57659174e7ef04
Netanel Basal <netanel7799@gmail.com>
2018-07-06 17:07:13 +0300

    feat(mock-component): add identifier option for easier query
    fix #34

--- src/lib/src/mock-directive.ts
+++ src/lib/src/mock-directive.ts
@@ -1,4 +1,4 @@
-import { Directive, EventEmitter } from '@angular/core';
+import { Directive, EventEmitter, Type } from '@angular/core';
 
 /**
  * Examples:
@@ -6,9 +6,10 @@ import { Directive, EventEmitter } from '@angular/core';
  * MockDirective({ selector: 'some-directive', inputs: ['some-input', 'some-other-input'] });
  *
  */
-export function MockDirective(options: Directive): Directive {
-  const metadata: Directive = {
+export function MockDirective(options: Directive & { identifier?: Type<any> }): Directive {
+  const metadata: Directive & { identifier?: Type<any> } = {
     selector: options.selector,
+    identifier: options.identifier,
     inputs: options.inputs,
     outputs: options.outputs || [],
     exportAs: options.exportAs || ''
@@ -19,6 +20,10 @@ export function MockDirective(options: Directive): Directive {
   metadata.outputs.forEach(method => {
     Mock.prototype[method] = new EventEmitter<any>();
   });
+
+  if (options.identifier) {
+    metadata.providers = [{ provide: options.identifier, useClass: Mock }];
+  }
 
   return Directive(metadata)(Mock as any);
 }
