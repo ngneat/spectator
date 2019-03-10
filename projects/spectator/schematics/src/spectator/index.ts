16c1a7800ab149fcecfee79e15c04b1ce76ffa41
Rex <rexebin@gmail.com>
2019-03-10 08:45:57 +0000

    Fix: ensure path if undefined

--- projects/spectator/schematics/src/spectator/index.ts
+++ projects/spectator/schematics/src/spectator/index.ts
@@ -1,7 +1,7 @@
 import { apply, chain, externalSchematic, MergeStrategy, mergeWith, move, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 import { ComponentOptions, DirectiveOptions, ServiceOptions } from './schema';
-import { normalize, strings } from '@angular-devkit/core';
-
+import { experimental, normalize, strings } from '@angular-devkit/core';
+import { getWorkspace } from '@schematics/angular/utility/config';
 export function spectatorComponentSchematic(options: ComponentOptions): Rule {
   return chain([
     externalSchematic('@schematics/angular', 'component', {
@@ -10,6 +10,7 @@ export function spectatorComponentSchematic(options: ComponentOptions): Rule {
       spec: false
     }),
     (_tree: Tree, _context: SchematicContext): Rule => {
+      _ensurePath(tree, options);
       const movePath = normalize(options.path + '/' + strings.dasherize(options.name) || '');
       const specTemplateRule = apply(url(`./files/${options.withHost ? 'component-host' : options.withCustomHost ? 'component-custom-host' : 'component'}`), [
         template({
@@ -31,6 +32,7 @@ export function spectatorServiceSchematic(options: ServiceOptions): Rule {
       spec: false
     }),
     (_tree: Tree, _context: SchematicContext): Rule => {
+      _ensurePath(tree, options);
       const movePath = normalize(options.path || '');
       const specTemplateRule = apply(url(`./files/${options.isDataService ? 'data-service' : `service`}`), [
         template({
@@ -52,6 +54,7 @@ export function spectatorDirectiveSchematic(options: DirectiveOptions): Rule {
       spec: false
     }),
     (_tree: Tree, _context: SchematicContext): Rule => {
+      _ensurePath(tree, options);
       const movePath = normalize(options.path || '');
       const specTemplateRule = apply(url(`./files/directive`), [
         template({
@@ -63,4 +66,20 @@ export function spectatorDirectiveSchematic(options: DirectiveOptions): Rule {
       return mergeWith(specTemplateRule, MergeStrategy.Default);
     }
   ]);
+}
+
+
+function _ensurePath(tree: Tree, options: any){
+  const workspace: experimental.workspace.WorkspaceSchema = getWorkspace(tree);
+  if (!options.project) {
+    options.project = Object.keys(workspace.projects)[0];
+  }
+  const project = workspace.projects[options.project];
+  if (options.path === undefined) {
+    const root = project.sourceRoot
+      ? `/${project.sourceRoot}/`
+      : `/${project.root}/src/app`;
+    const projectDirName = project.projectType === 'application' ? 'app' : 'lib';
+    options.path = `${root}${projectDirName}`;
+  }
 }
