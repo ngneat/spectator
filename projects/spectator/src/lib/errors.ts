2d325108611307e3b87a221ee8c0f0f1b2eb4619
Martin Nuc <martin@nuc.cz>
2019-03-12 09:01:40 +0100

    feat(lib): handle not found elemenet by.directive

--- projects/spectator/src/lib/errors.ts
+++ projects/spectator/src/lib/errors.ts
@@ -1,1 +1,14 @@
-export class SpectatorError extends Error {}
+export class SpectatorError extends Error {
+  constructor(message: string) {
+    super(message);
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, SpectatorError.prototype);
+  }
+}
+export class SpectatorDebugElementNotFoundError extends SpectatorError {
+  constructor(message: string) {
+    super(message);
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, SpectatorDebugElementNotFoundError.prototype);
+  }
+}
