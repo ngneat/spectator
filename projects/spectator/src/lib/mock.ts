32bac7d1c1765f17872bbb39d5d6891a893dfd7c
Martin Nuc <martin@nuc.cz>
2019-03-13 18:48:24 +0100

    fix(lib): avoid mockProvider to modify its inputs

--- projects/spectator/src/lib/mock.ts
+++ projects/spectator/src/lib/mock.ts
@@ -35,7 +35,7 @@ export function installProtoMethods(mock: any, proto: any, createSpyFn: Function
 }
 
 export function createSpyObject<T>(type: Type<T>, template?: Partial<Record<keyof T, any>>): SpyObject<T> {
-  const mock: any = template || {};
+  const mock: any = Object.assign({}, template) || {};
 
   return installProtoMethods(mock, type.prototype, name => {
     const newSpy: CompatibleSpy = jasmine.createSpy(name) as any;
