9edad7d1c68cf399768b5d7cbe4e7f21f598cfca
Netanel Basal <netanel7799@gmail.com>
2019-05-07 21:45:36 +0300

    fix(event-objects): add missing code

--- projects/spectator/src/lib/event-objects.ts
+++ projects/spectator/src/lib/event-objects.ts
@@ -6,6 +6,10 @@ export function createMouseEvent(type: string, x = 0, y = 0, button = 0) {
 
   event.initMouseEvent(type, true /* canBubble */, false /* cancelable */, window /* view */, 0 /* detail */, x /* screenX */, y /* screenY */, x /* clientX */, y /* clientY */, false /* ctrlKey */, false /* altKey */, false /* shiftKey */, false /* metaKey */, button /* button */, null /* relatedTarget */);
 
+  // `initMouseEvent` doesn't allow us to pass the `buttons` and
+  // defaults it to 0 which looks like a fake event.
+  Object.defineProperty(event, 'buttons', { get: () => 1 });
+
   return event;
 }
 
