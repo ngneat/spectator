de985e1251d1ff6bf47abc45cbf764ae5fe74632
Dirk Luijk <mail@dirkluijk.nl>
2019-08-08 22:54:33 +0200

    test: fixed tests

--- projects/spectator/jest/src/http.ts
+++ projects/spectator/jest/src/http.ts
@@ -1,6 +1,6 @@
-import { Type } from '@angular/core';
+import { FactoryProvider, Type } from '@angular/core';
 import { createHTTPFactory as baseCreateHTTPFactory, SpectatorHTTP } from '@netbasal/spectator';
 
-export function createHTTPFactory<T>(dataService: Type<T>, providers = []): () => SpectatorHTTP<T> {
+export function createHTTPFactory<T>(dataService: Type<T>, providers: FactoryProvider[] = []): () => SpectatorHTTP<T> {
   return baseCreateHTTPFactory(dataService, providers) as () => SpectatorHTTP<T>;
 }
