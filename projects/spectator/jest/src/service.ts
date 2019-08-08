7a8a010952a99812892dcc70a8e2579d9f5b36af
Netanel Basal <netanel7799@gmail.com>
2019-08-08 09:31:07 +0300

    feat(lib): v4 changes

--- projects/spectator/jest/src/service.ts
+++ projects/spectator/jest/src/service.ts
@@ -1,7 +1,12 @@
-import { Type } from '@angular/core';
-import { createService as baseCreateService, ServiceParams, SpectatorService as BaseSpectatorService } from '@netbasal/spectator';
+import { InjectionToken, Type } from '@angular/core';
+import { createService as baseCreateService, ServiceParams, SpectatorService as BaseSpectatorService, Token } from '@netbasal/spectator';
+import { SpyObject } from './mock';
 
-export interface SpectatorService<Service> extends BaseSpectatorService<Service> {}
+export interface SpectatorService<S> extends BaseSpectatorService<S> {
+  get<T>(token: Type<T> | InjectionToken<T>): T & SpyObject<T>;
+
+  get<T>(token: Token<T> | Token<any>): T & SpyObject<T>;
+}
 
 export function createService<Service>(options: ServiceParams<Service> | Type<Service>): SpectatorService<Service> {
   return baseCreateService(options);
