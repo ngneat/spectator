6270640e7025afe2aff01c3458d34a2df6029ec3
Dirk Luijk <mail@dirkluijk.nl>
2019-08-23 00:03:51 +0200

    fix(mock): allow assigning read-only properties

--- projects/spectator/jest/test/spy-object/spy-object.spec.ts
+++ projects/spectator/jest/test/spy-object/spy-object.spec.ts
@@ -9,10 +9,18 @@ describe('SpyObject', () => {
     person.sayHi.andReturn('Bye!');
   });
 
-  it('should keep getters/setters', () => {
+  it('should enable spying on properties', () => {
     const person = createSpyObject(Person);
     person.birthYear = 1990;
     jest.spyOn(person, 'age', 'get').mockReturnValue(29);
+
+    expect(person.age).toBe(29);
+  });
+
+  it('should enable setting properties by just assigning', () => {
+    const person = createSpyObject(Person);
+    person.birthYear = 1990;
+    (person as any).age = 29;
 
     expect(person.age).toBe(29);
   });
