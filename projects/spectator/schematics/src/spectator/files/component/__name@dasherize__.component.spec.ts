9f2a8496509adb26bb17633a9e8f8d033a378cf8
Dirk Luijk <mail@dirkluijk.nl>
2019-08-26 19:30:30 +0200

    fix(schematics): update schematics for v4 API changes

--- projects/spectator/schematics/src/spectator/files/component/__name@dasherize__.component.spec.ts
+++ projects/spectator/schematics/src/spectator/files/component/__name@dasherize__.component.spec.ts
@@ -1,12 +1,15 @@
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
+
 import { <%= classify(name)%>Component } from './<%= dasherize(name)%>.component';
-import { Spectator, createTestComponentFactory } from '@ngneat/spectator';
 
 describe('<%= classify(name)%>Component', () => {
   let spectator: Spectator<<%= classify(name)%>Component>;
-  const createComponent = createTestComponentFactory(<%= classify(name)%>Component);
+
+  const createComponent = createComponentFactory(<%= classify(name)%>Component);
 
   it('should create', () => {
     spectator = createComponent();
+
     expect(spectator.component).toBeTruthy();
   });
 });
