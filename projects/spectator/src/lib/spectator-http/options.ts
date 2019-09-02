99c2314e04b584a52ef6babc6af565e462560772
Dirk Luijk <mail@dirkluijk.nl>
2019-09-02 11:10:47 +0200

    fix(http): rename data service option to service

--- projects/spectator/src/lib/spectator-http/options.ts
+++ projects/spectator/src/lib/spectator-http/options.ts
@@ -4,8 +4,21 @@ import { getDefaultBaseOptions, BaseSpectatorOptions } from '../base/options';
 import { merge } from '../internals/merge';
 import { OptionalsRequired } from '../types';
 
-export interface SpectatorHttpOptions<S = any> extends BaseSpectatorOptions {
+interface SpectatorHttpOptionsDeprecated<S = any> extends BaseSpectatorOptions {
+  /**
+   * @deprecated. Please use service property instead.
+   */
   dataService: Type<S>;
+}
+
+interface SpectatorHttpOptionsNew<S = any> extends BaseSpectatorOptions {
+  service: Type<S>;
+}
+
+export type SpectatorHttpOptions<S = any> = SpectatorHttpOptionsDeprecated<S> | SpectatorHttpOptionsNew<S>;
+
+export function isDeprecated(options: SpectatorHttpOptions): options is SpectatorHttpOptionsDeprecated {
+  return (options as any).dataService !== undefined;
 }
 
 const defaultHttpOptions: OptionalsRequired<SpectatorHttpOptions> = {
