fa76cf91d04cf1dbfe382f878f85a18cb1d3d0e7
Yoeri Nijs <yoeri@posteo.net>
2019-12-12 16:01:03 +0100

    fix: let key parser handle single dot

--- projects/spectator/test/events/events.component.spec.ts
+++ projects/spectator/test/events/events.component.spec.ts
@@ -9,17 +9,42 @@ describe('EventsComponent', () => {
 
   beforeEach(() => (spectator = createComponent()));
 
-  it('should change the text on focus/blur', () => {
+  it('should handle focus', () => {
     spectator.focus('input');
     expect(spectator.query('h1')).toHaveText('focus');
+  });
+
+  it('should handle blur', () => {
     spectator.blur('input');
     expect(spectator.query('h1')).toHaveText('blur');
+  });
 
+  it('should handle single key press', () => {
     spectator.keyboard.pressKey('a', 'input');
     expect(spectator.query('h1')).toHaveText('pressed a');
+  });
+
+  it('should handle key with single modifier', () => {
     spectator.keyboard.pressKey('ctrl.a', 'input');
     expect(spectator.query('h1')).toHaveText('pressed ctrl.a');
+  });
+
+  it('should handle key with multiple modifiers', () => {
     spectator.keyboard.pressKey('ctrl.shift.a', 'input');
     expect(spectator.query('h1')).toHaveText('pressed ctrl.shift.a');
+  });
+
+  it('should handle a single dot correctly', () => {
+    spectator.keyboard.pressKey('.', 'input');
+    expect(spectator.query('h1')).toHaveText('pressed dot');
+  });
+
+  it('should fail when the pressKey sequence contains only dots', () => {
+    try {
+      spectator.keyboard.pressKey('...', 'input');
+      fail('Expected that an error is thrown');
+    } catch (e) {
+      expect(e.message).toEqual('invalid key modifier: undefined, keyStr: ...');
+    }
   });
 });
