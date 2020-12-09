9887bdb5e5e19fc14f2511d81f3c6515a4090b51
Tom Stones <stones@users.noreply.github.com>
2020-12-10 00:31:01 +1100

    fix(selectors): forgot to expose byrole selector (#376)

--- projects/spectator/jest/src/lib/dom-selectors.ts
+++ projects/spectator/jest/src/lib/dom-selectors.ts
@@ -1,1 +1,1 @@
-export { byAltText, byLabel, byPlaceholder, byText, byTextContent, byTitle, byValue, byTestId } from '@ngneat/spectator';
+export { byAltText, byLabel, byPlaceholder, byText, byTextContent, byTitle, byValue, byTestId, byRole } from '@ngneat/spectator';
