f7b2f6445ea6bbc554035ae5270a546c59e95b61
John Crim <johncrim@users.noreply.github.com>
2021-08-09 22:37:49 -0700

    Bug/373 jest focus (#473)

--- projects/spectator/src/lib/internals/element-focus.ts
+++ projects/spectator/src/lib/internals/element-focus.ts
@@ -1,13 +1,73 @@
 import { dispatchFakeEvent } from '../dispatch-events';
+import { isRunningInJsDom } from '../utils';
+
+/** Property added to HTML Elements to ensure we don't double-patch focus methods on an element. */
+const IS_FOCUS_PATCHED_PROP = Symbol('isFocusPatched');
+
+/** Ensures that a single set of matching focus and blur events occur when HTMLElement.focus() is called. */
+class FocusEventWatcher implements EventListenerObject {
+
+  private readonly priorActiveElement: Element | null;
+
+  /** Set to true when browser sends a blur event for priorActiveElement */
+  private _blurred = false;
+  /** Set to true when browser sends a focus event for element */
+  private _focused = false;
+
+  constructor(private readonly element: HTMLElement) {
+    this.element.addEventListener('focus', this);
+    this.priorActiveElement = element.ownerDocument.activeElement;
+    this.priorActiveElement?.addEventListener('blur', this);
+  }
+
+  public handleEvent({ type }: Event): void {
+    if (type === 'focus') {
+      this._focused = true;
+    }
+    else if (type === 'blur') {
+      this._blurred = true;
+    }
+  }
+
+  /**
+   * If focus and blur events haven't occurred, fire fake ones.
+   */
+  public ensureFocusEvents() {
+    this.element.removeEventListener('focus', this);
+    this.priorActiveElement?.removeEventListener('blur', this);
+
+    // Ensure priorActiveElement is blurred
+    if (!this._blurred && this.priorActiveElement) {
+      dispatchFakeEvent(this.priorActiveElement, 'blur');
+    }
+
+    if (!this._focused) {
+      dispatchFakeEvent(this.element, 'focus'); // Needed to cause focus event
+    }
+  }
+}
 
 /**
- * Patches an elements focus and blur methods to emit events consistently and predictably.
+ * Patches an element's focus and blur methods to emit events consistently and predictably in tests.
  * This is necessary, because some browsers, like IE11, will call the focus handlers asynchronously,
  * while others won't fire them at all if the browser window is not focused.
  *
  * patchElementFocus(triggerEl);
  */
 export function patchElementFocus(element: HTMLElement): void {
-  element.focus = () => dispatchFakeEvent(element, 'focus');
-  element.blur = () => dispatchFakeEvent(element, 'blur');
+
+  // https://github.com/ngneat/spectator/issues/373 - Don't patch when using JSDOM, eg in Jest
+  if (!isRunningInJsDom() && (element[IS_FOCUS_PATCHED_PROP] === undefined)) {
+    const originalFocus = element.focus.bind(element);
+    element.focus = (options) => {
+      const focusEventWatcher = new FocusEventWatcher(element);
+
+      // Sets document.activeElement. May or may not send focus + blur events
+      originalFocus(options);
+
+      focusEventWatcher.ensureFocusEvents();
+    }
+    element.blur = () => dispatchFakeEvent(element, 'blur');
+    element[IS_FOCUS_PATCHED_PROP] = true;
+  }
 }
---
+++ projects/spectator/test/focus/test-focus.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-focus',
+  template: `<button id="button1" (focus)="countFocus('button1')" (blur)="countBlur('button1')">Button1</button>
+             <button id="button2" (focus)="countFocus('button2')" (blur)="countBlur('button2')">Button2</button>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[attr.tabindex]': '0',
+    '(focus)': 'countFocus("app-test-focus")',
+    '(blur)': 'countBlur("app-test-focus")'
+  }
+})
+export class TestFocusComponent {
+
+  private readonly focusCounts = new Map<string, number>();
+  private readonly blurCounts = new Map<string, number>();
+
+  public countFocus(id: string) {
+    this.focusCounts.set(id, this.focusCount(id) + 1);
+  }
+
+  public countBlur(id: string) {
+    this.blurCounts.set(id, this.blurCount(id) + 1);
+  }
+
+  public focusCount(id: string): number {
+    return this.focusCounts.get(id) ?? 0;
+  }
+
+  public blurCount(id: string): number {
+    return this.blurCounts.get(id) ?? 0;
+  }
+
+}
