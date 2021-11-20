7c54f8587003d7e719bd3a01764766522148c049
wSedlacek <wsedlacekc@gmail.com>
2021-11-20 11:02:52 -0800

    feat!: support angular v13
    BREAKING CHANGE: Angular 13 ships packages with esm modules insteald of umd.

--- projects/spectator/test/test.ts
+++ projects/spectator/test/test.ts
@@ -19,7 +19,9 @@ defineGlobalsInjections({
 });
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: false }
+});
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
