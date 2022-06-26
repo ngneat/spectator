46d526144021f11dea485038a9b988411af0963c
Netanel Basal <NetanelBasal@users.noreply.github.com>
2022-06-26 15:14:26 +0300

    feat: 🎸 support jest esm (#565)
    The library will assume that Jest is used when Jasmine is not defined

--- projects/spectator/src/lib/core.ts
+++ projects/spectator/src/lib/core.ts
@@ -1,15 +1,15 @@
 import { CustomMatcherFactory } from './matchers';
 
 declare var jasmine: any;
-declare var jest: any;
 
 export function addMatchers(matchers: Record<string, CustomMatcherFactory>): void {
   if (!matchers) return;
 
   if (typeof jasmine !== 'undefined') {
     jasmine.addMatchers(matchers);
-  }
-  if (typeof jest !== 'undefined') {
+  } else {
+    // Jest isn't on the global scope when using ESM so we
+    // assume that it's Jest if Jasmine is not defined
     const jestExpectExtend = {};
     for (const key of Object.keys(matchers)) {
       if (key.startsWith('to')) jestExpectExtend[key] = matchers[key]().compare;
