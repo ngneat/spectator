c521d32c8bf541e8bac77eba8b55e62054657917
arturovt <arthurandrosovich@gmail.com>
2022-07-22 21:49:58 +0300

    fix: unpatch `setTimeout` before importing jQuery so it is not captured by zone.js

--- projects/spectator/src/lib/matchers.ts
+++ projects/spectator/src/lib/matchers.ts
@@ -1,7 +1,22 @@
 /** Credit: https://github.com/unindented/custom-jquery-matchers/tree/master/packages/custom-jquery-matchers */
 /* eslint-disable no-shadow, @typescript-eslint/no-shadow */
 
-import $ from 'jquery';
+// This is done to prevent tests, that are being run in a `fakeAsync` zone, from failing randomly.
+// jQuery setups a timer internally if the `document.readyState` is `complete` by doing
+// `window.setTimeout(jQuery.ready)` (see its source code). Unit tests might fail randomly that
+// there're still timers in the queue, but the timer has been scheduled by jQuery, so we don't
+// want to allow that to happen.
+const patchedSetTimeout = window.setTimeout;
+// The unpatched `setTimeout` can be retrieved through this property.
+// We might have used `Zone.__symbol__('OriginalDelegate')`, which would also give us
+// the current string, but accessing `Zone` requires messing up with types (like declaring it
+// globally through `declare const Zone`). This is the safest way of doing that.
+// This is done before importing jQuery since it will use the unpatched timer
+// when its code is executed, which will not be captured by zone.js.
+window.setTimeout = patchedSetTimeout['__zone_symbol__OriginalDelegate'] || patchedSetTimeout;
+// Note: do not use `import` since imports are hoisted during the compilation.
+const $ = require('jquery');
+window.setTimeout = patchedSetTimeout;
 
 import { hex2rgb, isHex, trim } from './internals/rgb-to-hex';
 import { isHTMLOptionElementArray, isObject } from './types';
