1e4e3f5116071f6bad84016eab1c0b3b4307aad8
Guillaume Nury <nury.guillaume@gmail.com>
2022-09-19 10:45:39 +0200

    test(dom-spectator): add query with root tests

---
+++ projects/spectator/test/query-root/query-root.component.spec.ts
@@ -0,0 +1,34 @@
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+
+import { QueryRootComponent, QueryRootOverlayComponent } from './query-root.component';
+
+describe('QueryRootComponent', () => {
+  let spectator: Spectator<QueryRootComponent>;
+  const createComponent = createComponentFactory(QueryRootComponent);
+
+  beforeEach(() => {
+    spectator = createComponent();
+  });
+
+  it('should allow querying one by directive', () => {
+    spectator.component.openOverlay();
+
+    const component = spectator.query(QueryRootOverlayComponent, { root: true });
+    expect(component?.title).toBe('Query Root Overlay');
+  });
+
+  it('should allow querying last by directive', () => {
+    spectator.component.openOverlay();
+
+    const component = spectator.queryLast(QueryRootOverlayComponent, { root: true });
+    expect(component?.title).toBe('Query Root Overlay');
+  });
+
+  it('should allow querying all by directive', () => {
+    spectator.component.openOverlay();
+
+    const components = spectator.queryAll(QueryRootOverlayComponent, { root: true });
+    expect(components.length).toBe(1);
+    expect(components[0].title).toBe('Query Root Overlay');
+  });
+});
