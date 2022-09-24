4fddc46e221450358a353c340447a8ac2e15f37b
va-stefanek <mateusz.stefanczyk@valueadd.pl>
2022-09-24 22:12:18 +0200

    feat(components): allow to override standalone components

---
+++ projects/spectator/jest/test/override-component.spec.ts
@@ -0,0 +1,110 @@
+import { createComponentFactory, createHostFactory, Spectator, SpectatorHost } from '@ngneat/spectator';
+import { Component } from '@angular/core';
+import { QueryService } from '../../test/query.service';
+import { overrideComponents } from '../../src/lib/spectator/create-factory';
+
+// Created only for testing purpose
+@Component({
+  selector: `app-standalone-with-dependency`,
+  template: `<div id="standalone">Standalone component with dependency!</div>`,
+  standalone: true,
+})
+export class StandaloneComponentWithDependency {
+  constructor(public query: QueryService) {}
+}
+
+@Component({
+  selector: `app-standalone-with-import`,
+  template: `<div id="standalone">Standalone component with import!</div>
+    <app-standalone-with-dependency></app-standalone-with-dependency>`,
+  imports: [StandaloneComponentWithDependency],
+  standalone: true,
+})
+export class StandaloneWithImportsComponent {}
+
+@Component({
+  selector: `app-standalone-with-dependency`,
+  template: `<div id="standaloneWithDependency">Standalone component with override dependency!</div>`,
+  standalone: true,
+})
+export class MockStandaloneComponentWithDependency {
+  constructor() {}
+}
+
+@Component({
+  selector: `app-non-standalone`,
+  template: `<div id="standalone">Non standalone</div>`,
+})
+export class MockNonStandaloneComponent {
+  constructor() {}
+}
+
+describe('Override Component', () => {
+  it('should throw error when override non standalone component', () => {
+    expect(() =>
+      overrideComponents({
+        overrideComponents: [
+          [
+            MockNonStandaloneComponent,
+            {
+              remove: { imports: [] },
+              add: { imports: [] },
+            },
+          ],
+        ],
+      } as any)
+    ).toThrowError('Can not override non standalone component');
+  });
+
+  describe('with Spectator', () => {
+    let spectator: Spectator<StandaloneWithImportsComponent>;
+
+    const createComponent = createComponentFactory({
+      component: StandaloneWithImportsComponent,
+      overrideComponents: [
+        [
+          StandaloneWithImportsComponent,
+          {
+            remove: { imports: [StandaloneComponentWithDependency] },
+            add: { imports: [MockStandaloneComponentWithDependency] },
+          },
+        ],
+      ],
+    });
+
+    beforeEach(() => {
+      spectator = createComponent();
+    });
+
+    it('should render a StandaloneWithImportsComponent', () => {
+      expect(spectator.query('#standalone')).toContainText('Standalone component with import!');
+    });
+  });
+
+  describe('with SpectatorHost', () => {
+    let host: SpectatorHost<StandaloneWithImportsComponent>;
+
+    const createHost = createHostFactory({
+      component: StandaloneWithImportsComponent,
+      template: `<div><app-standalone-with-import></app-standalone-with-import></div>`,
+      overrideComponents: [
+        [
+          StandaloneWithImportsComponent,
+          {
+            remove: { imports: [StandaloneComponentWithDependency] },
+            add: { imports: [MockStandaloneComponentWithDependency] },
+          },
+        ],
+      ],
+    });
+
+    beforeEach(() => {
+      host = createHost();
+    });
+
+    it('should render a StandaloneWithImportsComponent', () => {
+      expect(host.query('#standalone')).toContainText('Standalone component with import!');
+      expect(host.query('#standaloneWithDependency')).toContainText('Standalone component with override dependency!');
+    });
+  });
+});
---
+++ projects/spectator/jest/test/override-directive.spec.ts
@@ -0,0 +1,68 @@
+import {
+  createDirectiveFactory,
+  SpectatorDirective,
+} from '@ngneat/spectator';
+import { Directive, Inject, InjectionToken } from '@angular/core';
+import { overrideDirectives } from "../../src/lib/spectator/create-factory";
+
+// Created only for testing purpose
+export const directiveProviderToken = new InjectionToken('DirectiveProviderToken');
+@Directive({
+  selector: `[appStandaloneDirectiveWithDependency]`,
+  standalone: true,
+  providers: [{ provide: directiveProviderToken, useValue: 'test' }],
+})
+export class StandaloneDirectiveWithDependency {
+  constructor(@Inject(directiveProviderToken) public provider: string) {}
+}
+
+@Directive({
+  selector: `app-non-standalone-directive`,
+})
+export class MockNonStandaloneDirective {
+  constructor() {}
+}
+
+describe('Override Directive', () => {
+  it('should throw error when override non standalone directive', () => {
+    expect(() =>
+      overrideDirectives({
+        overrideDirectives: [
+          [
+            MockNonStandaloneDirective,
+            {
+              remove: { imports: [] },
+              add: { imports: [] },
+            },
+          ],
+        ],
+      } as any)
+    ).toThrowError('Can not override non standalone directive');
+  });
+
+  describe('with Spectator', () => {
+    let spectator: SpectatorDirective<StandaloneDirectiveWithDependency>;
+
+    const createDirective = createDirectiveFactory({
+      directive: StandaloneDirectiveWithDependency,
+      overrideDirectives: [
+        [
+          StandaloneDirectiveWithDependency,
+          {
+            remove: { providers: [{ provide: directiveProviderToken, useValue: 'test' }] },
+            add: { providers: [{ provide: directiveProviderToken, useValue: 'fakeTest' }] },
+          },
+        ],
+      ],
+      template: `<div class="default" appStandaloneDirectiveWithDependency>Testing Directive Providers</div>`
+    });
+
+    beforeEach(() => {
+      spectator = createDirective();
+    });
+
+    it('should render a StandaloneDirectiveWithDependency', () => {
+      expect(spectator.directive.provider).toEqual('fakeTest');
+    });
+  });
+});
