d2fcdf821d7a1f4f0c1dd0bb61f935aeb22b8bf2
Mateusz Stefańczyk <67691339+va-stefanek@users.noreply.github.com>
2022-11-13 14:32:41 +0100

    feat(query): allow to specify parent selector option (#590)

--- projects/spectator/test/dom-selectors/dom-selectors.component.ts
+++ projects/spectator/test/dom-selectors/dom-selectors.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-dom-selectors-nested-components',
+  template: `<p id="by-text-p">Nested Component</p>`,
+  standalone: true,
+})
+export class DomSelectorsNestedComponent {}
+
+@Component({
   selector: 'app-dom-selectors',
   template: `
     <p id="by-text-p">By text</p>
@@ -31,12 +38,22 @@ import { Component } from '@angular/core';
     </div>
 
     <div id="aria-checkboxes">
-      <section>
+      <section class="buttons-section">
         <button role="checkbox" aria-checked="true">Sugar</button>
         <button role="checkbox" aria-checked="false">Gummy bears</button>
         <button role="checkbox" aria-checked="false">Whipped cream</button>
       </section>
     </div>
-  `
+
+    <div id="nested-components-1">
+      <app-dom-selectors-nested-components id="alone-in-group"></app-dom-selectors-nested-components>
+    </div>
+    <div id="nested-components-2">
+      <app-dom-selectors-nested-components id="first"></app-dom-selectors-nested-components>
+      <app-dom-selectors-nested-components id="last"></app-dom-selectors-nested-components>
+    </div>
+  `,
+  standalone: true,
+  imports: [DomSelectorsNestedComponent]
 })
 export class DomSelectorsComponent {}
