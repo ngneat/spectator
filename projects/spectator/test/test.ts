458a76e6ebade4391acfd20330f13c99f4d3ab40
chimurai <655241+chimurai@users.noreply.github.com>
2022-12-21 06:39:08 +0000

    feat: 🎸 upgrade to Angular 15 (#594)
    * feat: 🎸 upgrade to Angular 15
    
    BREAKING CHANGE: 🧨 Angular 15
    BREAKING CHANGE: 🧨 The library now required a min version of Angular 15.0.3
    
    * feat: update karma test runner config for ng 15
    
    - update @angular-builders/jest to 15
    
    - update devkit karma settings to follow ng15 standards
    
    - run 14 to 15 migration for cli
    
    - add test file inclusion to angular.json
    
    - remove webpack code from test.ts
    
    Co-authored-by: Mark Bruning <mark.bruning@ensono.com>

--- projects/spectator/test/test.ts
+++ projects/spectator/test/test.ts
@@ -1,28 +1,9 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-import 'core-js/es/reflect';
-import 'zone.js';
-import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
 import { defineGlobalsInjections } from '../src/lib/globals-injections';
 
 import { TranslatePipe } from './translate.pipe';
 import { TranslateService } from './translate.service';
 
-declare const require: any;
-
 defineGlobalsInjections({
   providers: [TranslateService],
   declarations: [TranslatePipe],
 });
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: true },
-});
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
