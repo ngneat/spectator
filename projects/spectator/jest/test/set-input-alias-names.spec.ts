c07ac8cb3e28ab2ec1de566bcdcde66060ec0472
Fanis Prodromou <prodromouf@gmail.com>
2024-03-21 08:47:00 +0200

    fix(spectator): set input accepts alias names

---
+++ projects/spectator/jest/test/set-input-alias-names.spec.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { createComponentFactory } from '@ngneat/spectator/jest';
+
+describe('SetInputAliasNames', () => {
+  @Component({
+    selector: 'app-root',
+    template: `
+      <div data-test="set-input--name">{{ name }}</div>
+      <div data-test="set-input--age">{{ numOfYears }}</div>
+    `,
+    standalone: true,
+  })
+  class DummyComponent {
+    @Input('userName') name!: string;
+    @Input({ alias: 'age' }) numOfYears!: number;
+  }
+
+  const createComponent = createComponentFactory(DummyComponent);
+
+  it('setInput should respect the alias names', () => {
+    // Arrange
+    const spectator = createComponent();
+
+    const nameElement = spectator.query('[data-test="set-input--name"]')!;
+    const ageElement = spectator.query('[data-test="set-input--age"]')!;
+
+    // Act
+    spectator.setInput('userName', 'John');
+    spectator.setInput('age', '123');
+
+    // Assert
+    expect(nameElement.innerHTML).toBe('John');
+    expect(ageElement.innerHTML).toBe('123');
+  });
+});
