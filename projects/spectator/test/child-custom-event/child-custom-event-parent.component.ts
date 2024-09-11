d5124aabe5818c044d0b603d1ab38ccaff6d0001
arturovt <arthurandrosovich@gmail.com>
2024-09-11 18:49:09 +0300

    feat: allow `OutputEmitterRef` keys in `triggerEventHandler`

--- projects/spectator/test/child-custom-event/child-custom-event-parent.component.ts
+++ projects/spectator/test/child-custom-event/child-custom-event-parent.component.ts
@@ -3,14 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-child-custom-event-parent',
   template: `
-    <app-child-custom-event (customEvent)="onCustomEvent($event)"></app-child-custom-event>
+    <app-child-custom-event
+      (customEventUsingEventEmitter)="onCustomEventUsingEventEmitter($event)"
+      (customEventUsingOutputEmitter)="onCustomEventUsingOutputEmitter($event)"
+    />
     <p>{{ eventValue }}</p>
   `,
 })
 export class ChildCustomEventParentComponent {
   public eventValue = '';
 
-  public onCustomEvent(eventValue: string): void {
+  public onCustomEventUsingEventEmitter(eventValue: string): void {
+    this.eventValue = eventValue;
+  }
+
+  public onCustomEventUsingOutputEmitter(eventValue: string): void {
     this.eventValue = eventValue;
   }
 }
